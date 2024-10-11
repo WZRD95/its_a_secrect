4a9afff58468bc089923a31e0c0527e52a4ac6d0
root <root@WZRDPC>
2024-10-10 20:31:24 -0700

    testing my own VER 10/10/2024

---
+++ tests/endpoint.test.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const expect = require("chai").expect;
+const dockerBridgeIP = "172.17.0.1";
+
+describe("Tests to the \"/\" endpoint", () => {
+    it("should return a 200 status code", async () => {
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        expect(res.status).to.equal(200);
+    });
+
+    it("should return a JSON object with a Message", async () => {
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        expect(res.data).to.haveOwnProperty("message");
+    });
+
+    it("should return a JSON object with a Timestamp", async () => {
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        expect(res.data).to.haveOwnProperty("timestamp");
+    });
+
+    it("should return a Message saying \"My name is ...\"", async () => {
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        expect(res.data.message).to.contain("My name is");
+    });
+
+    it("should return a UNIX style timestamp (numerical values only)", async () => {
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        expect(res.data.timestamp).to.be.a("number");
+    });
+    it("should return a timestamp within a few seconds of now", async () => {
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
+        const now = Date.now();
+        expect(res.data.timestamp).to.be.within(now - 5000, now);
+    });
+    it("should return from My Private REPO", async () => {
+        //const res = await axios(`http://${dockerBridgeIP}:80/`);
+        // TODO: Finish this test
+        expect("TEST").to.contain("TEST");
+    });
+});
